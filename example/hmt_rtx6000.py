61f43c428ac098a8a3cd24a3a1987fac288847f1
huilin66 <23039356r@connect.polyu.hk>
2026-07-08 10:43:07 +0800

    refactor(example): simplify YoloManager init and enable all split jobs
    replace explicit check call with init parameter, enable all dataset split jobs and add cube directory processing

--- example/hmt_rtx6000.py
+++ example/hmt_rtx6000.py
@@ -7,10 +7,7 @@ def yolo_split(input_dir):
     sta_dir = os.path.join(input_dir, "stats")
     os.makedirs(sta_dir, exist_ok=True)
 
-    mgr = YoloManager(input_dir, layout="flat")
-
-    # 校验
-    mgr.check(out=os.path.join(sta_dir, "validation.json"))
+    mgr = YoloManager(input_dir, layout="flat", init_check=os.path.join(sta_dir, "validation.json"), init_check_fill_missing_txt=True)
 
     # 统计
     mgr.stats(
@@ -21,12 +18,16 @@ def yolo_split(input_dir):
     mgr.dataset_split(train=0.9, val=0.1, test=0.0, seed=233, absolute_paths=True)
 
 if __name__ == "__main__":
+    pass
     rgb_all_dir = r"/localnvme/data/bdd_hmt/sua_rgb"
     rgb_all_rgbt_dir = r"/localnvme/data/bdd_hmt/sua_rgb_rgbt"
     t_all_dir = r"/localnvme/data/bdd_hmt/sua_t"
     t_all_rgbt_dir = r"/localnvme/data/bdd_hmt/sua_t_rgbt"
+    cube_dir = r"/localnvme/data/bdd_hmt/bp_cube"
+
 
     yolo_split(rgb_all_dir)
-    # yolo_split(rgb_all_rgbt_dir)
-    # yolo_split(t_all_dir)
-    # yolo_split(t_all_rgbt_dir)
+    yolo_split(rgb_all_rgbt_dir)
+    yolo_split(t_all_dir)
+    yolo_split(t_all_rgbt_dir)
+    yolo_split(cube_dir)
