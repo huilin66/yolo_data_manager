b3b4c8897d4e4a266734e774af016878b4a677dc
huilin66 <23039356r@connect.polyu.hk>
2026-08-04 11:06:53 +0800

    feat: Refactor yolo_error_ana and yolo_metric functions for improved structure and add class exclusion capabilities

--- example/result_error_ana.py
+++ example/result_error_ana.py
@@ -5,32 +5,30 @@ from yolo_data_manager import YoloManager
 
 def yolo_error_ana(input_dir, pred_dir, pred_name, abs_path=False, workers=8):
     if not abs_path:
-        pred_dir = os.path.join(input_dir, pred_dir, 'labels')
-
-    ana_dir = os.path.join(input_dir, "ana", pred_name)
-    os.makedirs(ana_dir, exist_ok=True)
+        pred_dir = os.path.join(input_dir, pred_dir, "labels")
 
     mgr = YoloManager(input_dir, layout="auto", init_check=False)
 
-    # split
+    ana_dir = os.path.join(mgr.root, "result_ana", pred_name)
     mgr.eval_error_analysis(
         pred_root=pred_dir,
-        out=ana_dir, 
-        match_iou=0.5, 
-        low_iou=0.1,
+        out=ana_dir,
         conf_thres=0.01,
-        duplicate_iou=0.9,
+        # match_iou=0.5,
+        # low_iou=0.1,
+        # duplicate_iou=0.9,
         review=True,
         crop_padding=12,
         review_workers=workers,
         review_progress=True,
         review_progress_leave=False,
         copy_pred_txt=True,
-        )
+        workers=workers,
+    )
+
 
 if __name__ == "__main__":
-    pass
-    data_dir = r'/localnvme/project/ultralytics/ultralytics/cfg/datasets_traffic_sign/tf_defect_3.yaml'
-    pred_dir = r"/localnvme/project/ultralytics/runs/detect"
+    data_dir = r"/localnvme/project/ultralytics/ultralytics/cfg/datasets_hmt/hmt_t.yaml"
+    pred_dir = r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect"
 
-    yolo_error_ana(data_dir, pred_dir, "val-23")
+    yolo_error_ana(data_dir, pred_dir, "val-52")
--- example/result_metric.py
+++ example/result_metric.py
@@ -3,26 +3,54 @@ import os
 from yolo_data_manager import YoloManager
 
 
-def yolo_metric(input_dir, pred_dir, pred_name, abs_path=False, workers=8):
+def yolo_metric(
+    input_dir,
+    pred_dir,
+    pred_name,
+    abs_path=False,
+    workers=8,
+    class_=None,
+    exclude_class_=None,
+    merge_class_map=None,
+):
     if not abs_path:
         pred_dir = os.path.join(pred_dir, pred_name, "labels")
 
     mgr = YoloManager(input_dir, layout="auto", init_check=False)
-    ana_dir = os.path.join(mgr.root, pred_name)
+    ana_dir = os.path.join(mgr.root, "result_ana", pred_name)
     mgr.eval_metrics(
         pred_root=pred_dir,
-        # class_=["fire", "smoke"],   # 可选：只评估指定类别；也可用 [0, 1]
-        # min_pixels=8,               # 可选：过滤小目标
-        conf_thres=0.1,  # 可选：置信度过滤
+        class_=class_,  # 可选：只评估指定类别；也可用 [0, 1]
+        exclude_class_=exclude_class_,
+        merge_class_map=merge_class_map,
+        # min_pixels=8,  # 可选：过滤小目标
+        conf_thres=0.001,  # 可选：置信度过滤
         out=os.path.join(ana_dir, "metrics.json"),
         csv=os.path.join(ana_dir, "metrics.csv"),
         print_table=True,
+        show_original=True,
+        workers=workers,
     )
 
 
 if __name__ == "__main__":
-    pass
-    data_dir = r"/localnvme/project/ultralytics/ultralytics/cfg/datasets_traffic_sign/tf_defect_3.yaml"
-    pred_dir = r"/localnvme/project/ultralytics/runs/detect"
+    data_dir = r"/localnvme/project/ultralytics/ultralytics/cfg/datasets_hmt/hmt_t.yaml"
+    pred_dir = r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect"
 
-    yolo_metric(data_dir, pred_dir, "val-23")
+    yolo_metric(
+        data_dir,
+        pred_dir,
+        "val-52",
+        # class_=["Hollow High Risk Line"],
+        exclude_class_=["Hollow High Risk Line"],
+        merge_class_map={
+            "Hollow": [
+                "Hollow Low Risk",
+                "Hollow High Risk",
+            ],
+            "Temperature": [
+                "Temperature Medium Risk",
+                "Temperature High Risk",
+            ],
+        },
+    )
