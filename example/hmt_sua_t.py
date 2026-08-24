fe0e2ef800ba65fa950ca4bb06f22dfe3a98804b
huilin66 <23039356r@connect.polyu.hk>
2026-08-24 16:31:26 +0800

    feat: update dataset paths and add support for class-based updates in main function

--- example/hmt_sua_t.py
+++ example/hmt_sua_t.py
@@ -23,37 +23,54 @@ from example.functions import (
     yolo_error_ana,
     yolo_metric,
     yolo_resize,
+    yolo_split,
     yolo_sta,
+    yolo_update_by_label,
     yolo_update_by_pred,
     yolo_update_class,
     yolo_vis,
 )
 
+# HMT_V2_DIR = r"/localnvme/data/bdd_hmt/hmt_t_update_v2"
+HMT_V3_DIR = r"/localnvme/data/bdd_hmt/hmt_t_update_v3"
+HMT_V4_DIR = r"/localnvme/data/bdd_hmt/hmt_t_update_v4"
+# DATA_DIR = Path(
+#     r"/localnvme/project/ultralytics/ultralytics/cfg/datasets_hmt/hmt_t.yaml"
+# )
 DATA_DIR = Path(
-    r"/localnvme/project/ultralytics/ultralytics/cfg/datasets_hmt/hmt_t.yaml"
+    r"/localnvme/project/ultralytics/ultralytics/cfg/datasets_hmt/hmt_t_update_v4.yaml"
 )
-
 
 PRED_RUNS_DIR = Path(r"/localnvme/project/aic_mdet/models/ultralytics/runs/detect")
 # PRED_NAME = "val-161"
 PRED_NAMES = [
-    "val-164",
-    "val-165",
-    "val-166",
-    "val-167",
-    "val-168",
-    "val-169",
+    "predict-9",
+    "predict-10",
+    # "val-219",
+    # "val-165",
+    # "val-166",
+    # "val-167",
+    # "val-168",
+    # "val-169",
 ]
-
+LEAKAGE_ONLY_LIST = (
+    "/localnvme/data/bdd_hmt/hmt_t_update_v3/train_leakage_loss_mask.txt"
+)
 PRED_DIR = "/localnvme/data/bdd_hmt/sua_t/ydm_evaluation/error_analysis/val-169/review/pred_txt"
-CROP_ROOT = (
+CROP_ROOT_LABEL = "/localnvme/data/bdd_hmt/sua_t/ydm_vis/crop_change"
+CROP_ROOT_PRED = (
     "/localnvme/data/bdd_hmt/sua_t/ydm_evaluation/error_analysis/val-169/crop_change"
 )
-CROP_MAP = {
-    os.path.join(CROP_ROOT, "2_h_high"): "Hollow High Risk",
-    os.path.join(CROP_ROOT, "2_h_low"): "Hollow Low Risk",
-    os.path.join(CROP_ROOT, "none_2_h_high"): "Hollow High Risk",
-    os.path.join(CROP_ROOT, "none_2_h_low"): "Hollow Low Risk",
+
+CROP_MAP_LABEL = {
+    os.path.join(CROP_ROOT_LABEL, "2_broken"): "Broken",
+}
+
+CROP_MAP_PRED = {
+    os.path.join(CROP_ROOT_PRED, "2_h_high"): "Hollow High Risk",
+    os.path.join(CROP_ROOT_PRED, "2_h_low"): "Hollow Low Risk",
+    os.path.join(CROP_ROOT_PRED, "none_2_h_high"): "Hollow High Risk",
+    os.path.join(CROP_ROOT_PRED, "none_2_h_low"): "Hollow Low Risk",
 }
 MERGE_CLASS_MAP = {
     "Hollow": [
@@ -88,8 +105,10 @@ RUN_LIST = [
     # "error_ana",
     # "update",
     # "draw",
-    "resize",
-    "update_class",
+    # "resize",
+    # "update_class",
+    "update_class_by_label",
+    # "split"
 ]
 
 
@@ -102,6 +121,13 @@ def main() -> None:
 
     if "vis" in RUN_LIST:
         yolo_vis(DATA_DIR, crop=True)
+    if "update_class_by_label" in RUN_LIST:
+        for crops_dir, target_class in CROP_MAP_LABEL.items():
+            yolo_update_by_label(
+                DATA_DIR,
+                crops_dir=crops_dir,
+                to=target_class,
+            )
 
     if "metric" in RUN_LIST:
         for pred_name in PRED_NAMES[:]:
@@ -109,8 +135,8 @@ def main() -> None:
                 DATA_DIR,
                 PRED_RUNS_DIR,
                 pred_name,
-                merge_class_map=MERGE_CLASS_MAP,
-                # min_pixels=20,
+                # merge_class_map=MERGE_CLASS_MAP,
+                # min_pixels=50,
             )
 
     if "error_ana" in RUN_LIST:
@@ -119,11 +145,11 @@ def main() -> None:
                 DATA_DIR,
                 PRED_RUNS_DIR,
                 pred_name,
-                only_val=True,
+                # only_val=True,
             )
 
     if "update" in RUN_LIST:
-        for crops_dir, target_class in CROP_MAP.items():
+        for crops_dir, target_class in CROP_MAP_PRED.items():
             yolo_update_by_pred(
                 DATA_DIR,
                 crops_dir=crops_dir,
@@ -141,6 +167,9 @@ def main() -> None:
         )
     if "update_class" in RUN_LIST:
         yolo_update_class(DATA_DIR, class_map=UPDATE_CLASS_MAP)
+
+    if "split" in RUN_LIST:
+        yolo_split(HMT_V3_DIR, train_include_list=LEAKAGE_ONLY_LIST)
 
 
 if __name__ == "__main__":
